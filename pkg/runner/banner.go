4860fb083d707d2d2a00aa7221eb2d50dd2d6cc5
sandeep <8293321+ehsandeep@users.noreply.github.com>
2024-02-01 01:37:11 +0530

    version update

--- pkg/runner/banner.go
+++ pkg/runner/banner.go
@@ -17,7 +17,7 @@ const banner = `
 `
 
 // Version is the current version
-const Version = `v0.0.2`
+const Version = `v0.0.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
